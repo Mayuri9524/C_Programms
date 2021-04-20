627017fe6e562e2b0ff790b3c6472c442052f01e
Mayuri9524 <74652135+Mayuri9524@users.noreply.github.com>
2021-04-20 19:02:07 +0530

    Max-Min.c
    Algorithm for maximum
     
        START
            Accept first number as no1
            Accept second number as no2
     
            if no1 is greater than no2
                then no1 is maximum
            otherwise
                no2 is maximum
     
        STOP

---
+++ Max-Min.c
@@ -0,0 +1,31 @@
+
+// Accept 2 numbers fro user and return the maximum and minimum number.
+
+# include "Header2.h"
+
+int main()
+{
+    int iNo1 = 0;
+    int iNo2 = 0;
+    int iRet = 0;
+    
+    printf("Enter first number\n");
+    scanf("%d",&iNo1);
+    
+    printf("Enter second number\n");
+    scanf("%d",&iNo2);
+    
+    iRet = Maximum(iNo1,iNo2);
+    
+    printf("Maximum number is : %d\n",iRet);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
